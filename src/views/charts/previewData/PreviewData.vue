8cd1c83372a9cb363e80df680d32c152b87b7022
LnrdRmj <leonardo.ra14@icloud.com>
2024-05-09 14:22:58 +0200

    changed preview data

--- src/views/charts/previewData/PreviewData.vue
+++ src/views/charts/previewData/PreviewData.vue
@@ -13,16 +13,16 @@ const props = defineProps<{
         <div class="font-bold text-2xl">Statistic Results</div>
 
         <div class="space-y-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-            <SingleData name="largest sale" :value="data.max_sales_price_gt"/>
+            <SingleData name="Largest sale" :value="data.max_sales_price_gt.toLocaleString()"/>
             <SingleData name="Lower estimation avg" :value="data.total_estimation_min_value_all.toLocaleString()"/>
             <SingleData name="Higher estimation avg" :value="data.total_estimation_max_value_all.toLocaleString()"/>
             <SingleData name="Total sales amount" :value="data.total_sales_price_sold.toLocaleString()"/>
             <SingleData name="Sales amount avg" :value="data.average_sales.toLocaleString()"/>
-            <SingleData name="Smallest sale" :value="data.min_sold.toLocaleString()"/>
+            <SingleData name="Smallest sale" :value="data.total_min_sold.toLocaleString()"/>
             <div class="h-[2px] bg-black w-full sm:col-span-full"></div>
             <SingleData name="Chassis code" :value="data.unsold_vehicles.toLocaleString()"/>
             <SingleData name="With Quotation" :value="data.count_with_estimations.toLocaleString()"/>
-            <SingleData name="Brand count" :value="data.ratio_sales.toLocaleString()"/>
+            <SingleData name="Brand count" :value="data.total_ratio_sales.toLocaleString()"/>
             <SingleData name="Vehicle ount" :value="data.count_vehicle_sales_all.toLocaleString()"/>
         </div>
 
