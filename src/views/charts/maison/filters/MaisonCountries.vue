caad2c13e42d3ddd3a3b33bd31a021539036ccf5
LnrdRmj <leonardo.ra14@icloud.com>
2024-05-11 16:00:45 +0200

    countries filters for maison
    to complete

---
+++ src/views/charts/maison/filters/MaisonCountries.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Countries from '../../components/filters/Countries.vue';
+
+const continents: string[] = []
+const selectedContinent = ref<string | null>(null)
+const selectedCountries = ref<string[]>([])
+const countriesOfContinent = ref<any[]>([])
+
+</script>
+
+<template>
+
+    <Countries
+        :continents="continents"
+        v-model="selectedCountries"
+        v-model:selectedContinent="selectedContinent"
+        v-model:countriesOfContinent="countriesOfContinent"/>
+
+</template>
