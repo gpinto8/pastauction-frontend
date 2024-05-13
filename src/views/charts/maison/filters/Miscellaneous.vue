cba67493fa003be7cd893fc193235808e76b31d6
LnrdRmj <leonardo.ra14@icloud.com>
2024-05-13 17:05:42 +0200

    misc component

--- src/views/charts/maison/filters/Miscellaneous.vue
+++ src/views/charts/maison/filters/Miscellaneous.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts" generic="T extends { [key: string]: { name: string, value: string }[] }">
 import GenericFilter from '../../components/GenericFilter.vue';
-import { reactive } from 'vue';
+import { reactive, defineModel } from 'vue';
 
 const props = defineProps<{
     filterName?: string,
@@ -11,18 +11,22 @@ const selection = defineModel<{
     [keyof in T]: string
 }>({ required: true })
 
+function toggleSelection(optionName: string, value: string) {
+    selection.value[optionName] = selection.value[optionName] == value ? selection.value = null : selection.value = value
+}
+
 </script>
 
 <template>
     <GenericFilter :filterName="filterName || 'Miscellaneous'">
-        <div class="flex flex-wrap">
-            <div class="flex space-x-4 mr-4" v-for="(misc, key) of miscellaneousOptions">
+        <div class="flex flex-wrap flex-col sm:flex-row space-y-4 sm:space-y-0">
+            <div class="flex flex-wrap space-x-4 mr-4" v-for="(misc, optionName) of miscellaneousOptions">
                 <v-btn
                     v-for="option of misc"
-                    class="letter-button"
-                    :variant="selection[key] === option.value ? 'elevated' : 'outlined'"
-                    @click="selection[key] = option.value"
-                    :color="selection[key] === option.value ? 'black' : ''"
+                    class="letter-button flex-1 sm:w-fit"
+                    :variant="selection[optionName] === option.value ? 'elevated' : 'outlined'"
+                    @click="toggleSelection(optionName, option.value)"
+                    :color="selection[optionName] === option.value ? 'black' : ''"
                     style="min-width: 20px; margin: 2px; border-radius: 0px; font-size: 10px;"
                 >
                     {{ option.name }}
