cd088c32bd81b6b79363bd7f716811a717185baa
LnrdRmj <leonardo.ra14@icloud.com>
2024-05-16 12:56:51 +0200

    periods filter static data

--- src/views/charts/components/filters/Periods.vue
+++ src/views/charts/components/filters/Periods.vue
@@ -7,10 +7,22 @@ defineExpose({
     resetFilter
 })
 const props = defineProps<{
-    periods: any[],
+    // periods: any[],
     filterName?: string
 }>()
 const selectedPeriods = defineModel<string[]>({ required: true })
+
+const periods = {
+    Anitique: { startYear: 1880, endYear: 1904, },
+    Veteran: { startYear: 1905, endYear: 1918, },
+    Vintage: { startYear: 1919, endYear: 1930, },
+    'Post Vintage': { startYear: 1931, endYear: 1945, },
+    Classic: { startYear: 1946, endYear: 1964, },
+    'Posc Classic': { startYear: 1965, endYear: 1974, },
+    Modern: { startYear: 1975, endYear: 1999, },
+    Contemporary: { startYear: 2000, endYear: 2020, },
+    'N/A': { startYear: 1974, endYear: 1904, },
+}
 
 function resetFilter() {
     emptyArray(selectedPeriods.value)
@@ -21,20 +33,27 @@ function resetFilter() {
 <template>
     <div>
         <GenericFilter :filterName="filterName || 'Periods'" justify="start" light>
-            <div class="grid gap-2 grid-cols-2 
+            <div class="grid gap-2 grid-cols-2
                         sm:grid-cols-3 
                         md:grid-cols-4 
                         lg:flex lg:gap-0 lg:space-x-3 lg:flex-wrap">
                 <v-btn
-                    v-for="period in periods"
-                    :key="period.age_name"
-                    class="letter-button selection"
-                    :variant="selectedPeriods.includes(period.age_name) ? 'elevated' : 'outlined'"
-                    @click="toggleValueInArray(selectedPeriods, period.age_name)"
-                    :color="selectedPeriods.includes(period.age_name) ? 'black' : ''"
+                    v-for="(period, name) of periods"
+                    :key="name"
+                    class="letter-button selection !h-14"
+                    :variant="selectedPeriods.includes(name) ? 'elevated' : 'outlined'"
+                    @click="toggleValueInArray(selectedPeriods, name)"
+                    :color="selectedPeriods.includes(name) ? 'black' : ''"
                     style="min-width: 20px; margin: 2px; font-size: 10px;"
                 >
-                    {{ period.age_name }}
+                    <div class="flex flex-col">
+                        <div>
+                            {{ name }}
+                        </div>
+                        <div>
+                            {{ period.startYear }} - {{ period.endYear }}
+                        </div>
+                    </div>
                 </v-btn>
             </div>
         </GenericFilter>
