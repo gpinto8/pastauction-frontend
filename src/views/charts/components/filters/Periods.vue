612aa26abe69ec2671585c43358bc2c345f28fbc
LnrdRmj <leonardo.ra14@icloud.com>
2024-05-18 11:30:08 +0200

    periods in one line

--- src/views/charts/components/filters/Periods.vue
+++ src/views/charts/components/filters/Periods.vue
@@ -36,25 +36,25 @@ function resetFilter() {
             <div class="grid gap-2 grid-cols-2
                         sm:grid-cols-3 
                         md:grid-cols-4 
-                        lg:flex lg:gap-0 lg:space-x-3 lg:flex-wrap">
-                <v-btn
+                        lg:flex lg:gap-0 lg:space-x-3">
+                <button
                     v-for="(period, name) of periods"
                     :key="name"
-                    class="capitalize selection !h-14 !text-[13px] text-left"
-                    :variant="selectedPeriods.includes(name) ? 'elevated' : 'outlined'"
+                    class="capitalize selection !h-14 !text-[13px] text-left border !border-black p-2 grow shrink transition-colors"
+                    :class="selectedPeriods.includes(name) ? 'bg-black text-white' : ''"
                     @click="toggleValueInArray(selectedPeriods, name)"
                     :color="selectedPeriods.includes(name) ? 'black' : ''"
-                    style="min-width: 20px; margin: 2px; font-size: 10px; height: 50px !important; font-family: sans-serif !important"
+                    style="min-width: 20px; margin: 2px; font-family: sans-serif !important"
                 >
                     <div class="flex flex-col">
                         <div>
                             {{ name }}
                         </div>
                         <div>
-                            {{ name == 'N/A' ? 'none' : `${period.startYear} - ${period.endYear}` }}
+                            {{ name == 'N/A' ? 'none' : `${period.startYear}-${period.endYear}` }}
                         </div>
                     </div>
-                </v-btn>
+                </button>
             </div>
         </GenericFilter>
     </div>
