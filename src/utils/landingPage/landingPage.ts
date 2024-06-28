40650e20b025b15a7bed1bbd18ec5b63daa51e0e
LnrdRmj <leonardo.ra14@icloud.com>
2024-06-28 14:59:00 +0200

    head section is dynamic

--- src/utils/landingPage/landingPage.ts
+++ src/utils/landingPage/landingPage.ts
@@ -9,3 +9,18 @@ export function getPhotoBoxes(sections: Section[]) {
       .sort((a, b) => a.area_position - b.area_position)
   );
 }
+
+const HEAD_PAGE_AREA = 'head';
+export function getVideoPageArea(sections: Section[]) {
+  return sections.find(
+    section =>
+      section.page_area == HEAD_PAGE_AREA && section.media_type == 'video'
+  );
+}
+
+export function getHeadPageArea(sections: Section[]) {
+  return sections.filter(
+    section =>
+      section.page_area == HEAD_PAGE_AREA && section.media_type == 'text'
+  );
+}
--- src/views/homePage/LandingPage.vue
+++ src/views/homePage/LandingPage.vue
@@ -12,16 +12,15 @@ import BrandsSlider from './BrandsSlider.vue';
 import { groupIntoPairs } from '../../utils/functions/groupIntoPairs';
 import { homepageSections, type Section } from '../../api/landingPage/landingPage';
 import { computed, ref } from 'vue';
-import { getPhotoBoxes } from '@/utils/landingPage/landingPage';
+import { getHeadPageArea, getPhotoBoxes, getVideoPageArea } from '@/utils/landingPage/landingPage';
 
 const sections = ref<Section[]>([])
 
-const photoBoxes = computed(() => {
-    return getPhotoBoxes(sections.value)
-})
-const sectionDividedByPairs = computed(() => {
-    return groupIntoPairs(photoBoxes.value)
-})
+const photoBoxes = computed(() => getPhotoBoxes(sections.value))
+const sectionDividedByPairs = computed(() => groupIntoPairs(photoBoxes.value))
+
+const headVideo = computed(() => getVideoPageArea(sections.value))
+const headSubtitles = computed(() => getHeadPageArea(sections.value))
 
 homepageSections()
     .then(response => {
@@ -36,20 +35,17 @@ homepageSections()
         <full-page ref="fullpage" id="fullpage">
             <div class="section">
                 <div class="flex flex-col h-screen w-full relative">
-                    <video :src="LandingVideo" autoplay1 muted loop
+                    <video :src="headVideo?.media_path || headVideo?.media_aws_path!" autoplay1 muted loop
                         class="h-full w-full object-cover absolute -z-10"></video>
 
                     <Header class="z-10" />
 
                     <div class="flex-1 flex flex-col px-24 py-10 text-white">
-                        <div class="text-6xl mt-32 palatino-bold">
-                            Where classic car<br>
-                            enthusiasts find<br>
-                            their dream rides
+                        <div class="text-6xl mt-32 palatino-bold max-w-[500px]">
+                            {{ headVideo?.text_title }}
                         </div>
-                        <div class="mt-10">
-                            We provide you with tools to connect, explore, gather information and<br>
-                            engage in the world of historic automotive
+                        <div class="mt-10 max-w-[500px]">
+                            {{ headVideo?.text_description }}
                         </div>
 
                         <button class="bg-yellow-400 mt-10 w-fit px-5 py-2 flex text-white">
@@ -60,17 +56,8 @@ homepageSections()
                         <div class="flex-1"></div>
 
                         <div class="flex w-full justify-between text-4xl font-medium">
-                            <div class="flex flex-col">
-                                <div>1 milion+</div>
-                                <div>vehicles</div>
-                            </div>
-                            <div class="flex flex-col">
-                                <div>+9,000</div>
-                                <div>events</div>
-                            </div>
-                            <div class="flex flex-col">
-                                <div>$26B+</div>
-                                <div>turnover</div>
+                            <div class="flex flex-col" v-for="subtitle of headSubtitles">
+                                {{ subtitle.text_title }}
                             </div>
                         </div>
 
