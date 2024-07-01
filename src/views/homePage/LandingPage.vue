d759cd2dc4e1ab65c72057af6baea97e2410e9b6
LnrdRmj <leonardo.ra14@icloud.com>
2024-07-01 13:23:17 +0200

    muted videos optiosn

--- src/views/homePage/LandingPage.vue
+++ src/views/homePage/LandingPage.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
 
-import LandingVideo from './assets/PastAuctionLandingVideo.mp4'
-import Header from './Header.vue'
 import AppIcon from '@/components/common/AppIcon.vue';
+import Header from './Header.vue';
 
-import ImageSection1 from './assets/imageSection1.png'
-import ImageSection2 from './assets/imageSection2.png'
-import ImageSection3 from './assets/imageSection3.png'
-import ImageSection4 from './assets/imageSection4.png'
-import BrandsSlider from './BrandsSlider.vue';
-import { groupIntoPairs } from '../../utils/functions/groupIntoPairs';
-import { homepageSections, type Section } from '../../api/landingPage/landingPage';
-import { computed, ref } from 'vue';
 import { getCarouselCharts, getCarouselChartsBackgroundImage, getHeadPageArea, getPhotoBoxes, getVideoPageArea } from '@/utils/landingPage/landingPage';
+import { computed, ref } from 'vue';
+import { homepageSections, type Section } from '../../api/landingPage/landingPage';
+import { groupIntoPairs } from '../../utils/functions/groupIntoPairs';
+import BrandsSlider from './BrandsSlider.vue';
 import ChartsCarousel from './ChartsCarousel.vue';
+
 
 const sections = ref<Section[]>([])
 
@@ -22,6 +18,7 @@ const sectionDividedByPairs = computed(() => groupIntoPairs(photoBoxes.value))
 
 const headVideo = computed(() => getVideoPageArea(sections.value))
 const headSubtitles = computed(() => getHeadPageArea(sections.value))
+const muted = ref(true)
 
 const carouselCharts = computed(() => getCarouselCharts(sections.value))
 const carouselChartsBackgroundImage = computed(() => getCarouselChartsBackgroundImage(sections.value))
@@ -29,7 +26,6 @@ const carouselChartsBackgroundImage = computed(() => getCarouselChartsBackground
 homepageSections()
     .then(response => {
         sections.value = response.data.items
-
     })
 
 </script>
@@ -39,25 +35,37 @@ homepageSections()
         <full-page ref="fullpage" id="fullpage">
             <div class="section">
                 <div class="flex flex-col h-screen w-full relative overflow-hidden">
-                    <video :src="headVideo?.media_path || headVideo?.media_aws_path!" autoplay muted loop
-                        class="h-full w-full object-cover absolute -z-10"></video>
+                    <video :src="headVideo?.media_path || headVideo?.media_aws_path!" autoplay :muted="muted"
+                        class="h-full w-full object-cover absolute -z-10" />
 
                     <Header class="z-10" />
 
                     <div class="flex-1 flex flex-col px-24 py-10 text-white">
-                        <div class="flex flex-col animate-[fadeslidefromleft_1s_ease-in-out_forwards]">
-                            <div class="text-6xl mt-32 palatino-bold max-w-[500px]">
-                                {{ headVideo?.text_title }}
-                            </div>
-                            <div class="mt-10 max-w-[500px]">
-                                {{ headVideo?.text_description }}
-                            </div>
-                        </div>
+                        <div class="flex justify-between">
+                            <div class="flex flex-col">
+                                <div class="flex flex-col animate-[fadeslidefromleft_1s_ease-in-out_forwards]">
+                                    <div class="text-6xl mt-32 palatino-bold max-w-[500px]">
+                                        {{ headVideo?.text_title }}
+                                    </div>
+                                    <div class="mt-10 max-w-[500px]">
+                                        {{ headVideo?.text_description }}
+                                    </div>
+                                </div>
 
-                        <button class="bg-yellow-400 mt-10 w-fit px-5 py-2 flex text-white">
-                            <div>GET STARTED</div>
-                            <AppIcon class="-rotate-90 ml-3" type="chevron_down" />
-                        </button>
+                                <button class="bg-yellow-400 mt-10 w-fit px-5 py-2 flex text-white">
+                                    <div>GET STARTED</div>
+                                    <AppIcon class="-rotate-90 ml-3" type="chevron_down" />
+                                </button>
+                            </div>
+
+                            <div class="flex justify-end items-end mr-5" @click="muted = !muted">
+                                <div class="flex-center h-10">
+                                    <AppIcon type="sound_off" v-if="muted" />
+                                    <AppIcon type="sound_on" v-else />
+                                </div>
+                            </div>
+
+                        </div>
 
                         <div class="flex-1"></div>
 
