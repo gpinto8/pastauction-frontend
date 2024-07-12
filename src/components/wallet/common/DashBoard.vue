b784d850c3d831a8b9f348cf4733028426294702
Joaquin Esteban <dev@joacoesteban.com>
2024-07-12 20:54:02 +0200

    REFACTOR, linting

--- src/components/wallet/common/DashBoard.vue
+++ src/components/wallet/common/DashBoard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-// Import 
+// Import
 import { useGeneralStore } from '@/store/datas/general';
 import { onMounted } from 'vue';
 import { Swiper, SwiperSlide } from 'swiper/vue';
@@ -14,28 +14,42 @@ onMounted(() => {
   store.setTokens(300);
   store.setBolts(1500);
 });
-
 </script>
 
 <template>
   <div class="dash !hidden md:!flex shadow">
     <div class="token">
-      <h3><span class="uppercase">Tokens </span>available</h3>
+      <h3>
+        <span class="uppercase">Tokens</span>
+        available
+      </h3>
       <div class="flex items-center gap-2">
-        <img src="@/assets/images/bronze_token.png" alt="Bronze Token - Pastauction" class="w-14" />
+        <img
+          src="@/assets/images/bronze_token.png"
+          alt="Bronze Token - Pastauction"
+          class="w-14"
+        />
         <h3>+{{ store.getTokens }}</h3>
       </div>
     </div>
     <div class="plan">
       <div class="current flex gap-10">
         <h3>Free</h3>
-        <img src="@/assets/icons/info.svg" alt="Information - Pastauction" class="w-[18px]" />
+        <img
+          src="@/assets/icons/info.svg"
+          alt="Information - Pastauction"
+          class="w-[18px]"
+        />
       </div>
       <p class="text-sm text-grey"><i>Current plan</i></p>
     </div>
     <div class="bolts">
       <div class="flex items-center gap-1">
-        <img src="@/assets/images/bolt.png" alt="Bolt - Pastauction" class="h-[25px] rotate-[-90deg]"/>
+        <img
+          src="@/assets/images/bolt.png"
+          alt="Bolt - Pastauction"
+          class="h-[25px] rotate-[-90deg]"
+        />
         <h3>Bolts</h3>
       </div>
       <h3>+{{ store.getBolts }}</h3>
@@ -43,13 +57,13 @@ onMounted(() => {
   </div>
 
   <swiper
-  :slides-per-view="1.5"
-  :initialSlide=1
-  :space-between="50"
-  :centered-slides="true"
-  :pagination="{ clickable: false }"
-  class="md:!hidden"
-  style="padding-bottom: 50px; width: 100%"
+    :slides-per-view="1.5"
+    :initialSlide="1"
+    :space-between="50"
+    :centered-slides="true"
+    :pagination="{ clickable: false }"
+    class="md:!hidden"
+    style="padding-bottom: 50px; width: 100%"
   >
     <swiper-slide>
       <div class="card flex justify-between">
@@ -58,7 +72,11 @@ onMounted(() => {
           <h5>Available</h5>
         </div>
         <div class="flex items-center gap-2">
-          <img src="@/assets/images/bronze_token.png" alt="Bronze Token - Pastauction" class="w-14" />
+          <img
+            src="@/assets/images/bronze_token.png"
+            alt="Bronze Token - Pastauction"
+            class="w-14"
+          />
           <h3 class="font-bold">+{{ store.getTokens }}</h3>
         </div>
       </div>
@@ -80,27 +98,30 @@ onMounted(() => {
           <h5>Available</h5>
         </div>
         <div class="flex items-center gap-2">
-          <img src="@/assets/images/bronze_token.png" alt="Bronze Token - Pastauction" class="w-14" />
+          <img
+            src="@/assets/images/bronze_token.png"
+            alt="Bronze Token - Pastauction"
+            class="w-14"
+          />
           <h3 class="font-bold">+{{ store.getBolts }}</h3>
         </div>
       </div>
     </swiper-slide>
-
   </swiper>
 </template>
 
 <style scoped>
-  /* dash */
-  .dash {
-    @apply flex flex-col md:flex-row items-center justify-between py-6 px-10 gap-10;
+/* dash */
+.dash {
+  @apply flex flex-col md:flex-row items-center justify-between py-6 px-10 gap-10;
 
-    .token,
-    .plan,
-    .bolts {
-      @apply flex flex-col justify-around text-[24px] font-semibold;
-    }
+  .token,
+  .plan,
+  .bolts {
+    @apply flex flex-col justify-around text-[24px] font-semibold;
   }
-  .card{
-    background-color: rgba(255, 218, 68, 0.25);
-  }
+}
+.card {
+  background-color: rgba(255, 218, 68, 0.25);
+}
 </style>
