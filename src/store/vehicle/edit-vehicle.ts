acad1dca2bd4bbe53d86cd0371b8aa7b73482109
Joaquin Esteban <dev@joacoesteban.com>
2024-08-03 21:44:15 +0200

    FEAT, add edit-vehicle store

---
+++ src/store/vehicle/edit-vehicle.ts
@@ -0,0 +1,112 @@
+// In your vehicleStore.ts
+import { defineStore } from 'pinia'
+import { useVehicleStore, type Vehicle } from './vehicle'
+import { reactive, ref } from 'vue'
+import { debounceTime, Subject } from 'rxjs'
+import pinia from '@/store'
+
+export function mockVehicle (): Vehicle {
+  return {
+    "purchase_year": 0,
+    "purchase_value": 0,
+    "garage_set_id": "0",
+    "garage_choice": "",
+    "id_brand": 4855,
+    "id_family": 134213,
+    "model_id": 185437,
+    "location_id": "ITA",
+    "body_id": 1,
+    "exterior_color_id": 0,
+    "interior_color_id": 0,
+    "interior_type": "",
+    "variant": "",
+    "type_vehicle": "",
+    "year": 0,
+    "doors": "",
+    "transmission": "",
+    "power": "",
+    "engine_capacity": "",
+    "driver_side": "",
+    "originality": "",
+    "status": "",
+    "mileage": 0,
+    "original_miles": "",
+    "plate_numb": "",
+    "vin": "",
+    "main_photo": "",
+    "registration_certificate": "",
+    "service_book": "",
+    "purchase_currency": "",
+    "id": 0,
+    "logo": ""
+  }
+}
+
+export const useEditVehicleStore = defineStore('edit-vehicle', () => {
+  const vehicleStore = useVehicleStore()
+
+  const store = {
+    brands: ref<Array<{ name: string; id: number }>>([]),
+    families: ref([]),
+    models: ref([]),
+    bodies: ref([]),
+    loading: reactive({
+      brands: false,
+      families: false,
+      models: false,
+      bodies: false,
+    }),
+    vehicle: ref<Vehicle>(mockVehicle()),
+    async submit () {
+
+    },
+
+    async fetchInitialData () {
+      if (store.brands.value.length === 0) {  // Only fetch if not already loaded
+        const [brand_names, families, models, body] = await Promise.all([
+          vehicleStore.filter('bidwatcher_brand', 'name'),
+          vehicleStore.filter('bidwatcher_family', 'name'),
+          vehicleStore.filter('bidwatcher_model', 'family_a'),
+          vehicleStore.filter('bidwatcher_body', 'type'),
+        ]).then(res => res.map(r => r.items))
+
+        store.brands.value = brand_names
+        store.families.value = families
+        store.models.value = models
+        store.bodies.value = body
+      }
+    },
+    // ---
+    brandSearch$: new Subject<string>(),
+    async searchBrands (search: string) {
+      store.brandSearch$.next(search)
+    },
+    // ---
+    familySearch$: new Subject<string>(),
+    async searchFamilies (search: string) {
+      store.familySearch$.next(search)
+    },
+  }
+
+  // ----
+  store.brandSearch$.pipe(
+    debounceTime(100),
+  )
+    .subscribe(async (search) => {
+      store.loading.brands = true
+      const { items } = await vehicleStore.filter('bidwatcher_brand', 'name', search).finally(() => store.loading.brands = false)
+      store.brands.value = items
+    })
+  // ----
+  store.familySearch$.pipe(
+    debounceTime(100),
+  )
+    .subscribe(async (search) => {
+      store.loading.families = true
+      const { items } = await vehicleStore.filter('bidwatcher_family', 'name', search).finally(() => store.loading.families = false)
+      store.families.value = items
+    })
+
+
+  return store
+})
