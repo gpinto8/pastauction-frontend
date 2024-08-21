adc0fb2355a1bd1f3fb5dc81bfd21a897eb45288
LnrdRmj <leonardo.ra14@icloud.com>
2024-08-21 19:18:45 +0200

    utils

--- src/api/photo/photo.ts
+++ src/api/photo/photo.ts
@@ -1,3 +1,7 @@
 export function getImageFromPath(path: string) {
   return `https://pastauction.com/api/v1/photo/${path}`;
 }
+
+export function getCountryFlag(countryISO: string) {
+  return `https://past-auction-p.s3.amazonaws.com/LogoCountry/${countryISO}.jpeg`;
+}
