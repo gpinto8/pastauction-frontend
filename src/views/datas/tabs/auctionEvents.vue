4a5d8d9e5b89593a2d1e2490a9312b03525cc74d
Joaquin Esteban <dev@joacoesteban.com>
2024-08-22 15:58:59 +0200

    FEAT, implement reusable filters store in auctionEvents tab

--- src/views/datas/tabs/auctionEvents.vue
+++ src/views/datas/tabs/auctionEvents.vue
@@ -5,21 +5,21 @@
         <h6 class="font-bold text-lg">Event information</h6>
         <span>
           Event data:
-          {{ store.getListItems?.items[0]?.auction_data || '12/04/2023' }}
+          {{ store.queryItems[0]?.auction_data || '12/04/2023' }}
         </span>
         <div>
-          <img :src="store.getListItems?.items[0]?.flag_maison_url" />
-          {{ store.getListItems?.items[0]?.maison_name }}
+          <img :src="store.queryItems[0]?.flag_maison_url" />
+          {{ store.queryItems[0]?.maison_name }}
         </div>
         <div class="space-x-4">
-          <img :src="store.getListItems?.items[0]?.flag_auction_url" />
-          <span>{{ store.getListItems?.items[0]?.name_event }}</span>
+          <img :src="store.queryItems[0]?.flag_auction_url" />
+          <span>{{ store.queryItems[0]?.name_event }}</span>
           <a href="#" class="text-blue underline font-medium">Vehicles</a>
           <a href="#" class="text-blue underline font-medium">Event</a>
           <br />
           <p class="text-[#6C757D]">
             {{
-              store.getListItems?.items[0]?.auction_address ||
+              store.queryItems[0]?.auction_address ||
               'Gruga Hall, Norbertstrabe 2, 45131, Essen (DEU)'
             }}
           </p>
@@ -31,65 +31,55 @@
             <div>
               Vehicles on sale:
               <span class="border rounded sm p-1">
-                {{
-                  store.getListItems?.items[0]?.count_vehicle_sales_all || '-'
-                }}
+                {{ store.queryItems[0]?.count_vehicle_sales_all || '-' }}
               </span>
             </div>
             <div>
               Vehicles sold nr:
               <span class="border rounded sm p-1">
-                {{ store.getListItems?.items[0]?.total_auto_vendute || '-' }}
+                {{ store.queryItems[0]?.total_auto_vendute || '-' }}
               </span>
             </div>
             <div>
               Vehicles sold %
               <span class="border rounded sm p-1">
-                {{
-                  store.getListItems?.items[0]?.ratio_sales?.toFixed(2) || '-'
-                }}
+                {{ store.queryItems[0]?.ratio_sales || '-' }}
               </span>
             </div>
             <div>
               lower evaluation
               <span class="border rounded sm p-1">
-                {{ store.getListItems?.items[0]?.min_sold || '-' }}
+                {{ store.queryItems[0]?.min_sold || '-' }}
               </span>
             </div>
             <div>
               higher evaluation
               <span class="border rounded sm p-1">
-                {{ store.getListItems?.items[0]?.max_sales_price || '-' }}
+                {{ store.queryItems[0]?.max_sales_price || '-' }}
               </span>
             </div>
             <div>
               average sales price
               <span class="border rounded sm p-1">
-                {{
-                  store.getListItems?.items[0]?.average_sales?.toFixed(2) || '-'
-                }}
+                {{ store.queryItems[0]?.average_sales?.toFixed(2) || '-' }}
               </span>
             </div>
             <div>
               Vehicles evalueted
               <span class="border rounded sm p-1">
-                {{
-                  store.getListItems?.items[0]?.count_with_estimations || '-'
-                }}
+                {{ store.queryItems[0]?.count_with_estimations || '-' }}
               </span>
             </div>
             <div>
               Vehicles evalueted sold
               <span class="border rounded sm p-1">
-                {{ store.getListItems?.items[0]?.estimated_count_sales || '-' }}
+                {{ store.queryItems[0]?.estimated_count_sales || '-' }}
               </span>
             </div>
             <div>
               Vehicles "no reserve"
               <span class="border rounded sm p-1">
-                {{
-                  store.getListItems?.items[0]?.total_no_reserve_price || '-'
-                }}
+                {{ store.queryItems[0]?.total_no_reserve_price || '-' }}
               </span>
             </div>
           </div>
@@ -97,44 +87,44 @@
             <div>
               lots nr.
               <span class="border rounded sm p-1">
-                {{ store.getListItems?.items[0]?.lot_numeric || '-' }}
+                {{ store.queryItems[0]?.lot_numeric || '-' }}
               </span>
             </div>
             <div>
               lower lot nr
               <span class="border rounded sm p-1">
-                {{ store.getListItems?.items[0]?.lower_lot_number || '-' }}
+                {{ store.queryItems[0]?.lower_lot_number || '-' }}
               </span>
             </div>
             <div>
               higher lot nr
               <span class="border rounded sm p-1">
-                {{ store.getListItems?.items[0]?.higher_lot_number || '-' }}
+                {{ store.queryItems[0]?.higher_lot_number || '-' }}
               </span>
             </div>
             <div>
               teoretical_lots
               <span class="border rounded sm p-1">
-                {{ store.getListItems?.items[0]?.teoretical_lots || '-' }}
+                {{ store.queryItems[0]?.teoretical_lots || '-' }}
               </span>
             </div>
 
             <div>
               deviation_lots
               <span class="border rounded sm p-1">
-                {{ store.getListItems?.items[0]?.deviation_lots || '-' }}
+                {{ store.queryItems[0]?.deviation_lots || '-' }}
               </span>
             </div>
             <div>
               Vehicles with photo
               <span class="border rounded sm p-1">
-                {{ store.getListItems?.items[0]?.with_photo || '-' }}
+                {{ store.queryItems[0]?.with_photo || '-' }}
               </span>
             </div>
             <div>
               photo gallery nr
               <span class="border rounded sm p-1">
-                {{ store.getListItems?.items[0]?.count_galleries || '-' }}
+                {{ store.queryItems[0]?.count_galleries || '-' }}
               </span>
             </div>
           </div>
@@ -147,19 +137,19 @@
       <caption>Title</caption>
       <thead>
         <tr>
-          <th>{{ store.getListItems?.items[0]?.count_estimations_zero }}</th>
-          <th>{{ store.getListItems?.items[0]?.count_within_min_less_50 }}</th>
-          <th>{{ store.getListItems?.items[0]?.count_sales_lower_min_50 }}</th>
-          <th>{{ store.getListItems?.items[0]?.count_into_evaluetion }}</th>
-          <th>{{ store.getListItems?.items[0]?.count_sales_higher_max_50 }}</th>
+          <th>{{ store.queryItems[0]?.count_estimations_zero }}</th>
+          <th>{{ store.queryItems[0]?.count_within_min_less_50 }}</th>
+          <th>{{ store.queryItems[0]?.count_sales_lower_min_50 }}</th>
+          <th>{{ store.queryItems[0]?.count_into_evaluetion }}</th>
+          <th>{{ store.queryItems[0]?.count_sales_higher_max_50 }}</th>
           <th>
-            {{ store.getListItems?.items[0]?.count_higher_than_max_plus_50 }}
+            {{ store.queryItems[0]?.count_higher_than_max_plus_50 }}
           </th>
           <th>
-            {{ store.getListItems?.items[0]?.unsold_vehicles }}
+            {{ store.queryItems[0]?.unsold_vehicles }}
           </th>
           <th>
-            {{ store.getListItems?.items[0]?.number_vehicles }}
+            {{ store.queryItems[0]?.number_vehicles }}
           </th>
         </tr>
       </thead>
@@ -177,19 +167,17 @@
         <tr>
           <td></td>
           <td>
-            {{ store.getListItems?.items[0]?.count_within_min_less_50_100 }}
+            {{ store.queryItems[0]?.count_within_min_less_50_100 }}
           </td>
           <td>
-            {{ store.getListItems?.items[0]?.count_sales_lower_min_50_100 }}
+            {{ store.queryItems[0]?.count_sales_lower_min_50_100 }}
           </td>
-          <td>{{ store.getListItems?.items[0]?.count_into_evaluetion_100 }}</td>
+          <td>{{ store.queryItems[0]?.count_into_evaluetion_100 }}</td>
           <td>
-            {{ store.getListItems?.items[0]?.count_sales_higher_max_50_100 }}
+            {{ store.queryItems[0]?.count_sales_higher_max_50_100 }}
           </td>
           <td>
-            {{
-              store.getListItems?.items[0]?.count_higher_than_max_plus_50_100
-            }}
+            {{ store.queryItems[0]?.count_higher_than_max_plus_50_100 }}
           </td>
           <td></td>
           <td></td>
@@ -197,96 +185,15 @@
       </tbody>
     </table>
 
-    <div class="p-4 bg-white mb-4">
-      <div class="grid grid-cols-2 mb-4 justify-between items-center">
-        <div class="text-[#D80027]">
-          <span v-if="getLengthOfFilters < 3">
-            Please, select almost 3 parameters
-          </span>
-        </div>
-        <div class="text-right">
-          <v-btn
-            @click="paginate"
-            class="!bg-primary text-white !rounded-sm w-[130px]"
-          >
-            Run search
-          </v-btn>
-        </div>
-      </div>
-      <div class="grid grid-cols-7 gap-2">
-        <v-select
-          v-model="filters.auction_area"
-          label="Area"
-          item-title="area"
-          item-value="area"
-          variant="outlined"
-          density="compact"
-          :items="store.getListAreas"
-        />
-        <v-select
-          v-model="filters.country_auction_name"
-          label="Auction Country"
-          item-title="name"
-          item-value="name"
-          variant="outlined"
-          density="compact"
-          :return-object="false"
-          :items="store.getListCountries"
-        />
-        <v-select
-          v-model="filters.city_auction_name"
-          label="Auction City"
-          item-title="name"
-          item-value="name"
-          variant="outlined"
-          density="compact"
-          :items="store.getListCities"
-        />
-        <v-select
-          v-model="filters.name_event"
-          label="Auction Event"
-          item-title="name_event"
-          item-value="name_event"
-          variant="outlined"
-          density="compact"
-          :items="store.getListEvents"
-        />
-        <v-select
-          v-model="filters.maison_name"
-          label="Maison"
-          item-title="name"
-          item-value="name"
-          variant="outlined"
-          density="compact"
-          :items="store.getListMaison"
-        />
-        <v-select
-          v-model="filters.country_maison"
-          label="Maison Country"
-          item-title="area"
-          item-value="area"
-          variant="outlined"
-          density="compact"
-          :items="store.getListCountries"
-        />
-        <v-select
-          v-model="filters.auction_year"
-          label="Auction Year"
-          item-title="area"
-          item-value="area"
-          variant="outlined"
-          density="compact"
-          :items="store.getListYears"
-        />
-      </div>
-    </div>
+    <Filters :store="store" :headers="headers" />
+
     <div class="p-2 bg-white">
       <v-data-table
         v-model:items-per-page="itemsPerPage"
         :headers="headers as any"
-        :items-length="store?.getListItems?.total || 0"
-        :items="store.getListItems?.items || []"
-        :loading="store?.getLoadingListItems"
+        :items-length="store.queryItems?.total || 0"
+        :items="store.queryItems || []"
+        :loading="store?.loading.submit"
         class="elevation-1"
         item-value="name"
         density="compact"
@@ -319,36 +226,26 @@
               <tr>
                 <td class="!bg-red-500/20">
                   {{
-                    store.getListItems?.items[0]?.ratio_min_value_sales_sold?.toFixed(
-                      2
-                    )
+                    store.queryItems[0]?.ratio_min_value_sales_sold?.toFixed(2)
                   }}
                 </td>
                 <td>
-                  {{
-                    store.getListItems?.items[0]
-                      ?.total_estimation_min_value_sold
-                  }}
+                  {{ store.queryItems[0]?.total_estimation_min_value_sold }}
                 </td>
                 <td>
-                  {{
-                    store.getListItems?.items[0]
-                      ?.total_estimation_max_value_sold
-                  }}
+                  {{ store.queryItems[0]?.total_estimation_max_value_sold }}
                 </td>
                 <td class="!bg-red-500/20">
                   {{
-                    store.getListItems?.items[0]?.ratio_max_value_sales_sold?.toFixed(
-                      2
-                    )
+                    store.queryItems[0]?.ratio_max_value_sales_sold?.toFixed(2)
                   }}
                 </td>
 
                 <td>
-                  {{ store.getListItems?.items[0]?.total_sales_price_sold }}
+                  {{ store.queryItems[0]?.total_sales_price_sold }}
                 </td>
                 <td>
-                  {{ store.getListItems?.items[0]?.count_vehicle_sales_sold }}
+                  {{ store.queryItems[0]?.count_vehicle_sales_sold }}
                 </td>
               </tr>
             </tbody>
@@ -370,34 +267,26 @@
               <tr>
                 <td class="!bg-red-500/20">
                   {{
-                    store.getListItems?.items[0]?.ratio_min_value_sales_est?.toFixed(
-                      2
-                    )
+                    store.queryItems[0]?.ratio_min_value_sales_est?.toFixed(2)
                   }}
                 </td>
                 <td>
-                  {{
-                    store.getListItems?.items[0]?.total_estimation_min_value_est
-                  }}
+                  {{ store.queryItems[0]?.total_estimation_min_value_est }}
                 </td>
                 <td>
-                  {{
-                    store.getListItems?.items[0]?.total_estimation_max_value_est
-                  }}
+                  {{ store.queryItems[0]?.total_estimation_max_value_est }}
                 </td>
                 <td class="!bg-red-500/20">
                   {{
-                    store.getListItems?.items[0]?.ratio_max_value_sales_est?.toFixed(
-                      2
-                    )
+                    store.queryItems[0]?.ratio_max_value_sales_est?.toFixed(2)
                   }}
                 </td>
 
                 <td>
-                  {{ store.getListItems?.items[0]?.total_sales_price_est }}
+                  {{ store.queryItems[0]?.total_sales_price_est }}
                 </td>
                 <td>
-                  {{ store.getListItems?.items[0]?.count_vehicle_sales_est }}
+                  {{ store.queryItems[0]?.count_vehicle_sales_est }}
                 </td>
               </tr>
             </tbody>
@@ -419,34 +308,26 @@
               <tr>
                 <td class="!bg-red-500/20">
                   {{
-                    store.getListItems?.items[0]?.ratio_min_value_sales_all?.toFixed(
-                      2
-                    )
+                    store.queryItems[0]?.ratio_min_value_sales_all?.toFixed(2)
                   }}
                 </td>
                 <td>
-                  {{
-                    store.getListItems?.items[0]?.total_estimation_min_value_all
-                  }}
+                  {{ store.queryItems[0]?.total_estimation_min_value_all }}
                 </td>
                 <td>
-                  {{
-                    store.getListItems?.items[0]?.total_estimation_max_value_all
-                  }}
+                  {{ store.queryItems[0]?.total_estimation_max_value_all }}
                 </td>
                 <td class="!bg-red-500/20">
                   {{
-                    store.getListItems?.items[0]?.ratio_max_value_sales_all?.toFixed(
-                      2
-                    )
+                    store.queryItems[0]?.ratio_max_value_sales_all?.toFixed(2)
                   }}
                 </td>
 
                 <td>
-                  {{ store.getListItems?.items[0]?.total_sales_price_all }}
+                  {{ store.queryItems[0]?.total_sales_price_all }}
                 </td>
                 <td>
-                  {{ store.getListItems?.items[0]?.count_vehicle_sales_all }}
+                  {{ store.queryItems[0]?.count_vehicle_sales_all }}
                 </td>
               </tr>
             </tbody>
@@ -459,26 +340,31 @@
 
 <script setup lang="ts">
 import { ref, computed } from 'vue';
-import { useAuctionEventsStore } from '@/store/datas/auctionEvents';
+import { queryStore, Columns } from '@/store/datas/global';
+import Filters from '../filters.vue';
 
-const store = useAuctionEventsStore();
+const useStore = queryStore('query_1');
+const store = useStore();
+store.init();
 
-store.auctionAreas('bidwatcher_auction', 'area');
-store.auctionCountries('bidwatcher_country', 'name');
-store.auctionCities('bidwatcher_city', 'name');
-store.auctionMaison('bidwatcher_maison', 'name');
-store.auctionYear('bidwatcher_auction', 'year');
-store.auctionEvents('bidwatcher_auction', 'name_event');
+// const store = useAuctionEventsStore();
 
-const filters = ref({
-  auction_area: 'UK',
-  name_event: '',
-  country_auction_name: null,
-  country_maison: null,
-  maison_name: 'Bonhams',
-  city_auction_name: null,
-  auction_year: 2020,
-});
+// store.auctionAreas('bidwatcher_auction', 'area');
+// store.auctionCountries('bidwatcher_country', 'name');
+// store.auctionCities('bidwatcher_city', 'name');
+// store.auctionMaison('bidwatcher_maison', 'name');
+// store.auctionYear('bidwatcher_auction', 'year');
+// store.auctionEvents('bidwatcher_auction', 'name_event');
+
+// const filters = ref({
+//   auction_area: 'UK',
+//   name_event: '',
+//   country_auction_name: null,
+//   country_maison: null,
+//   maison_name: 'Bonhams',
+//   city_auction_name: null,
+//   auction_year: 2020,
+// });
 
 const sort = ref({
   auction_area: null,
@@ -564,20 +450,6 @@ const pager = ref<any>({
   page: 1,
   size: 10,
 });
-
-const getLengthOfFilters = computed(
-  () => Object.values(filters.value).filter(el => el !== null)?.length
-);
-
-const paginate = () => {
-  store.listPaginated(
-    pager.value.page,
-    pager.value.size,
-    filters.value,
-    sort.value
-  );
-};
-paginate();
 </script>
 
 <style lang="postcss">
