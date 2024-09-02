2df898ac4df31b5edc8e5b44cd377cdbe2d24f06
LnrdRmj <leonardo.ra14@icloud.com>
2024-09-02 19:12:58 +0200

    car angles

--- src/views/homePage/searchSection/SearchSection.vue
+++ src/views/homePage/searchSection/SearchSection.vue
@@ -4,6 +4,50 @@ import CarAngles from './assets/carAngles.png'
 import Data from './assets/data.png'
 import Car from './assets/car.png'
 
+import Angle1 from './assets/angles/angle-1.png';
+import Angle2 from './assets/angles/angle-2.png';
+import Angle3 from './assets/angles/angle-3.png';
+import Angle4 from './assets/angles/angle-4.png';
+import Angle5 from './assets/angles/angle-5.png';
+import Angle6 from './assets/angles/angle-6.png';
+import Angle7 from './assets/angles/angle-7.png';
+import Angle8 from './assets/angles/angle-8.png';
+import Angle9 from './assets/angles/angle-9.png';
+import Angle10 from './assets/angles/angle-10.png';
+import Angle11 from './assets/angles/angle-11.png';
+import Angle12 from './assets/angles/angle-12.png';
+import Angle13 from './assets/angles/angle-13.png';
+import Angle14 from './assets/angles/angle-14.png';
+import Angle15 from './assets/angles/angle-15.png';
+import Angle16 from './assets/angles/angle-16.png';
+import Angle17 from './assets/angles/angle-17.png';
+import Angle18 from './assets/angles/angle-18.png';
+import Angle19 from './assets/angles/angle-19.png';
+import Angle20 from './assets/angles/angle-20.png';
+
+const angles = [
+    Angle1,
+    Angle2,
+    Angle3,
+    Angle4,
+    Angle5,
+    Angle6,
+    Angle7,
+    Angle8,
+    Angle9,
+    Angle10,
+    Angle11,
+    Angle12,
+    Angle13,
+    Angle14,
+    Angle15,
+    Angle16,
+    Angle17,
+    Angle18,
+    Angle19,
+    Angle20,
+];
+
 </script>
 
 <template>
@@ -13,9 +57,14 @@ import Car from './assets/car.png'
             Advanced and detailed search
         </div>
         <div class="flex flex-1 min-h-0 py-10 justify-between gap-10">
-            <img :src="CarAngles" class="h-full" />
+            <!-- <img :src="CarAngles" class="h-full" /> -->
+            <div class="grid grid-cols-4 grid-rows-5 gap-1">
+                <div class="overflow-hidden" v-for="angle of angles">
+                    <img class="h-full object-cover hover:scale-110 transition-transform" :src="angle" />
+                </div>
+            </div>
             <div class="flex justify-end relative">
-                <img :src="Data" class="" />
+                <img :src="Data" />
             </div>
         </div>
         <div class="text-xs text-white/50">
