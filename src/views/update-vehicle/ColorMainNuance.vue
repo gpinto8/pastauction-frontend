75ecfa2a9fd279112a324eb1d6463ab1a66a2bda
gpinto8 <gpinto0910@gmail.com>
2024-12-26 02:57:09 +0100

    fix(vehicle-update): fix "Colours changes" not working due to the latest change

--- src/views/update-vehicle/ColorMainNuance.vue
+++ src/views/update-vehicle/ColorMainNuance.vue
@@ -43,7 +43,7 @@ watch(
   () => updateVehicleStore.selectedSubColor,
   () => {
     const color = updateVehicleStore.selectedSubColor;
-    handleSubColorSelection(color);
+    selectedSubColor.value = color;
   }
 );
 
